3b87af9fb774c8c4e74dd5b2f66bd7e251f8fadb
MierenManz <christiaanmens@gmail.com>
2022-05-13 12:05:38 +0200

    reduce memory usage by not cloning data

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 pub mod interpreter;
 
-use std::fs;
-
 use interpreter::Interpreter;
+use std::fs::OpenOptions;
+use std::io::Read;
 
 fn main() {
-	let src: String = fs::read_to_string("test.bf").expect("Problem opening file");
-	let rom: Vec<char> = src.chars().collect();
+	let mut rom = Vec::new();
+	{
+		let mut file_handle = OpenOptions::new()
+			.read(true)
+			.open("test.bf")
+			.expect("Problem opening file");
 
-    let mut interpreter = Interpreter::new(rom);
+		file_handle.read_to_end(&mut rom).unwrap();
+		// `file_handle` gets dropped after here and closes it
+	}
+
+	let mut interpreter = Interpreter::new(rom);
 
 	interpreter.run();
 }
-
